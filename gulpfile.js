3869c45b51e819954640df0087701aa7ce92a4b0
leo <leo@wizmacau.com>
2017-09-13 12:05:00 +0800

    add browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,45 @@
  * Created by leo on 2017/9/12.
  */
 
-var gulp = require('gulp');
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    minifyHtml= require("gulp-minify-html"),
+    reload      = browserSync.reload;
 
-gulp.task('default', function() {
-    // place code for your default task here
+// 静态服务器
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: "./web"
+    });
+});
+
+// 代理
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        proxy: "你的域名或IP"
+    });
+});
+
+gulp.task('minify-html',function() {
+    return gulp.src('app/*.html')
+        .pipe(minifyHtml())
+        .pipe(gulp.dest('web'))
+        .pipe(reload({
+            stream: true
+        }))
+});
+
+gulp.task('sass', function () {
+    return gulp.src('app/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('web/css'))
+        .pipe(reload({
+            stream: true
+        }))
+});
+
+gulp.task('watch', ['browserSync', 'minify-html', 'sass'], function () {
+    gulp.watch('app/scss/*.scss', ['sass']);
+    gulp.watch("app/*.html", ['minify-html']);
 });
